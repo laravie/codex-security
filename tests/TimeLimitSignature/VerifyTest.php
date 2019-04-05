475fe6d518955a343fe20f2bbdb19a1a9f66cde2
Mior Muhammad Zaki <crynobone@gmail.com>
2019-04-05 18:55:32 +0800

    Improve tests.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/TimeLimitSignature/VerifyTest.php
+++ tests/TimeLimitSignature/VerifyTest.php
@@ -53,4 +53,16 @@ class VerifyTest extends TestCase
             $stub('hello world', 't=1546300800,v1=99f58c8afc5bd7c61d7d509687224c8db8f300874b6471f85bcd0d34b5bd70fa')
         );
     }
+
+    /** @test */
+    public function it_cant_validate_signature_with_invalid_signed_value()
+    {
+        Carbon::setTestNow($now = Carbon::createFromTimestamp(1546301300));
+
+        $stub = new Verify('secret');
+
+        $this->assertFalse(
+            $stub('hello world', '99f58c8afc5bd7c61d7d509687224c8db8f300874b6471f85bcd0d34b5bd70fa')
+        );
+    }
 }
